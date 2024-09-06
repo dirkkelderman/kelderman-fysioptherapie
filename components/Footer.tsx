85d44aa90ac45e36da95ded622ac3ec231e1d1ae
Dirk <dirkkelderman@gmail.com>
2024-09-06 16:38:45 +0200

    more optimizing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,28 @@
 import { PrismicLink, PrismicRichText, PrismicText } from "@prismicio/react";
 import * as prismic from "@prismicio/client";
-
 import { Bounded } from "./Bounded";
 import { Heading } from "./Heading";
 
 const components = {
   heading3: ({ children }) => (
-    <Heading as="h3" size="sm" className="">
+    <Heading as="h3" size="sm" className="min-h-[40px]">
       {children}
     </Heading>
   ),
-  paragraph: ({ children }) => <p className="mb-1 ">{children}</p>,
+  paragraph: ({ children }) => <p className="mb-1">{children}</p>,
 };
 
 export const Footer = ({ footer }) => {
   return (
     <Bounded as="footer" yPadding="sm" className="bg-[#183540] text-white">
-      <div className="grid grid-cols-1 gap-x-6 gap-y-3 text-center leading-none md:grid-cols-4 ">
-        <div className="flex h-full min-h-[100px] flex-col items-center justify-between">
-          <PrismicRichText field={footer.data?.sitemapHeading} />
-          <ul className=" list-none">
+      <div className="grid grid-cols-1 gap-x-6 gap-y-3 text-center leading-none md:grid-cols-4">
+        {/* Sitemap */}
+        <div className="flex h-full min-h-[200px] flex-col items-center justify-between">
+          <PrismicRichText
+            field={footer.data?.sitemapHeading}
+            fallback={<h3>Loading...</h3>}
+          />
+          <ul className="min-h-[150px] list-none">
             {footer.data?.sitemap.map((item, index) => (
               <li key={index} className="tracking-tight text-white">
                 <PrismicLink field={item.link}>
@@ -29,20 +32,30 @@ export const Footer = ({ footer }) => {
             ))}
           </ul>
         </div>
-        <div className="flex h-full flex-col items-center justify-between">
-          <PrismicRichText field={footer.data?.locationsHeading} />
-          <ul className=" list-none">
+
+        {/* Locations */}
+        <div className="flex h-full min-h-[200px] flex-col items-center justify-between">
+          <PrismicRichText
+            field={footer.data?.locationsHeading}
+            fallback={<h3>Loading...</h3>}
+          />
+          <ul className="min-h-[150px] list-none">
             {footer.data?.locations.map((item, index) => (
-              <li key={index} className=" pb-2 tracking-tight text-white">
+              <li key={index} className="pb-2 tracking-tight text-white">
                 <PrismicRichText field={item.city} components={components} />
                 <PrismicRichText field={item.address} components={components} />
               </li>
             ))}
           </ul>
         </div>
-        <div className="flex h-full flex-col justify-between">
-          <PrismicRichText field={footer.data?.contactHeading} />
-          <ul className=" list-none">
+
+        {/* Contact */}
+        <div className="flex h-full min-h-[200px] flex-col justify-between">
+          <PrismicRichText
+            field={footer.data?.contactHeading}
+            fallback={<h3>Loading...</h3>}
+          />
+          <ul className="min-h-[150px] list-none">
             {footer.data?.contact.map((item, index) => (
               <li key={index} className="pb-2 tracking-tight text-white">
                 <PrismicRichText field={item.email} components={components} />
@@ -54,11 +67,16 @@ export const Footer = ({ footer }) => {
             ))}
           </ul>
         </div>
-        <div className="flex h-full flex-col justify-between">
-          <PrismicRichText field={footer.data?.socialsHeading} />
-          <ul className=" list-none">
+
+        {/* Socials */}
+        <div className="flex h-full min-h-[200px] flex-col justify-between">
+          <PrismicRichText
+            field={footer.data?.socialsHeading}
+            fallback={<h3>Loading...</h3>}
+          />
+          <ul className="min-h-[150px] list-none">
             {footer.data?.socials.map((item, index) => (
-              <li key={index} className=" tracking-tight text-white">
+              <li key={index} className="tracking-tight text-white">
                 <PrismicLink field={item.link}>
                   <PrismicText field={item.label} />
                 </PrismicLink>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,15 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
           rel="stylesheet"
         /> */}
+        {/* Preconnect links */}
+        <link rel="preconnect" href="https://prismic.io" />
+        <link rel="preconnect" href="https://static.cdn.prismic.io" />
+        <link rel="preconnect" href="https://vercel.live" />
+
+        {/* Optionally, add DNS prefetch to speed up connections even further */}
+        <link rel="dns-prefetch" href="https://prismic.io" />
+        <link rel="dns-prefetch" href="https://static.cdn.prismic.io" />
+        <link rel="dns-prefetch" href="https://vercel.live" />
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
