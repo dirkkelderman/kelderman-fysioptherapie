1162821ce2e83cd24471ca756f12b6693170b8a8
Dirk <dirkkelderman@gmail.com>
2024-12-23 16:34:43 +0100

    imrpoved seo with tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,18 +18,33 @@ const Index = ({ page, navigation, settings, footer }) => {
 
   return (
     <Layout navigation={navigation} settings={settings} footer={footer}>
+      <Head>
+        <meta name="generator" content="Kelderman Fysiotherapie" />
+        <meta name="application-name" content="Kelderman Fysiotherapie" />
+        <meta name="referrer" content="origin-when-cross-origin" />
+        <meta
+          name="keywords"
+          content="fysiotherapie, kelderman, therapie, behandeling, revalidatie, fysio"
+        />
+      </Head>
       <NextSeo
         title={metaTitle}
         description={metaDescription}
         canonical="https://www.keldermanfysiotherapie.nl/"
         openGraph={{
+          type: "website",
+          locale: "nl_NL",
+          url: "https://www.keldermanfysiotherapie.nl/",
+          siteName: "Kelderman Fysiotherapie",
           title: prismic.asText(socialCardTitle),
           description: prismic.asText(socialCardDescription),
           images: [
             {
-              url: "https://images.prismic.io/kelderman-fysiotherapie/dd404877-d58e-4264-8c1f-22f961cf710b_logo_groot_alleen_K.png?auto=compress,format",
-              width: 800,
-              height: 600,
+              url:
+                socialCardImage.url ||
+                "https://images.prismic.io/kelderman-fysiotherapie/dd404877-d58e-4264-8c1f-22f961cf710b_logo_groot_alleen_K.png?auto=compress,format",
+              width: 1200,
+              height: 630,
               alt: socialCardImage.alt,
               type: "image/png",
             },
